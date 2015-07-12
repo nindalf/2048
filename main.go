9c14fa4c8a1b2dd3c27026c33bfcec3853a69980
Krishna Sundarram <krishna.sundarram@gmail.com>
2015-07-12 22:29:44 +0530

    Add moveLeft() and moveRight()

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
+
+type grid [][]int
+
+func (g grid) moveRight() {
+	for i := range g {
+		// combine
+		for j := 0; j < len(g[i])-1; j++ {
+			if g[i][j] == g[i][j+1] {
+				g[i][j] = 0
+				g[i][j+1] *= 2
+				j++
+			} else if g[i][j+1] == 0 {
+				g[i][j], g[i][j+1] = g[i][j+1], g[i][j]
+			}
+		}
+		// collapse
+		for j := len(g[i]) - 1; j > 0; j-- {
+			if g[i][j] == 0 {
+				g[i][j], g[i][j-1] = g[i][j-1], g[i][j]
+			}
+		}
+	}
+}
+
+func (g grid) moveLeft() {
+	for i := range g {
+		// combine
+		for j := len(g[i]) - 1; j > 0; j-- {
+			if g[i][j] == g[i][j-1] {
+				g[i][j] = 0
+				g[i][j-1] *= 2
+				j--
+			} else if g[i][j-1] == 0 {
+				g[i][j], g[i][j-1] = g[i][j-1], g[i][j]
+			}
+		}
+		// collapse
+		for j := 0; j < len(g[i])-1; j++ {
+			if g[i][j] == 0 {
+				g[i][j], g[i][j+1] = g[i][j+1], g[i][j]
+			}
+		}
+	}
+}
+
+func (g grid) String() string {
+	var b bytes.Buffer
+	for i := range g {
+		b.Write([]byte(fmt.Sprintf("%v\n", g[i])))
+	}
+	return b.String()
+}
 
 func main() {
-	fmt.Println("hello")
+	g := grid{[]int{2, 2, 4, 8}, []int{2, 4, 4, 4}, []int{0, 8, 0, 2}, []int{0, 4, 0, 4}}
+	fmt.Println(g)
+	g.moveLeft()
+	fmt.Println(g)
 }
